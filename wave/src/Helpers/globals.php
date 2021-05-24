49421bc43a17c2df940a00c12a5a273fa6e80a03
Mohammad Javad <id4mjr@gmail.com>
2021-05-24 08:03:01 +0430

    Fix pgsql sequence after seeding

--- wave/src/Helpers/globals.php
+++ wave/src/Helpers/globals.php
@@ -112,3 +112,19 @@ if(!function_exists('tailwindPlanColor')){
 	}
 
 }
+
+if (!function_exists('fixPostgresSequence')) {
+
+    function fixPostgresSequence()
+    {
+        if (config('database.default') === 'pgsql') {
+            $tables = \DB::select('SELECT table_name FROM information_schema.tables WHERE table_schema = \'public\' ORDER BY table_name;');
+            foreach ($tables as $table) {
+                if (\Schema::hasColumn($table->table_name, 'id')) {
+                    $seq = \DB::table($table->table_name)->max('id') + 1;
+                    \DB::select('SELECT setval(pg_get_serial_sequence(\'' . $table->table_name . '\', \'id\'), coalesce(' . $seq . ',1), false) FROM ' . $table->table_name);
+                }
+            }
+        }
+    }
+}
