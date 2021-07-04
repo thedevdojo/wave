cfd3ddcfcd384f117dee9daa59ed01a5a929bc46
= <tnylea@gmail.com>
2021-07-04 13:04:57 -0400

    Adding bigInt to users table

--- wave/database/migrations/2018_09_22_234251_create_announcement_user_table.php
+++ wave/database/migrations/2018_09_22_234251_create_announcement_user_table.php
@@ -14,8 +14,8 @@ class CreateAnnouncementUserTable extends Migration {
 	{
 		Schema::create('announcement_user', function(Blueprint $table)
 		{
-			$table->integer('announcement_id')->unsigned();
-			$table->integer('user_id')->unsigned()->index();
+			$table->integer('announcement_id')->unsigned()->index();
+			$table->bigInteger('user_id')->unsigned()->index();
 		});
 	}
 
