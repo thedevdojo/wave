7d56024d938edbe47402b590b3d816866f7f9217
Marc Garcia Torrent <garciatorrent96@gmail.com>
2021-10-05 13:10:17 +0200

    Allow register for free plan

--- resources/views/themes/tailwind/partials/plans.blade.php
+++ resources/views/themes/tailwind/partials/plans.blade.php
@@ -42,6 +42,17 @@
                 </div>
 
                 <div class="relative">
+                    @if ($plan->price == 0 || strtolower($plan->price) == 'free')
+                        <a href="{{ route('register') }}" class="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition duration-150 ease-in-out @if($plan->default){{ ' bg-gradient-to-r from-wave-600 to-indigo-500 hover:from-wave-500 hover:to-indigo-400' }}@else{{ 'bg-gray-800 hover:bg-gray-700 active:bg-gray-900 focus:border-gray-900 focus:shadow-outline-gray' }}@endif border border-transparent cursor-pointer rounded-b-md checkout focus:outline-none disabled:opacity-25">
+                            @subscribed($plan->slug)
+                                Your subscribed to this plan
+                            @notsubscribed
+                                @notsubscriber
+                                    Get Started
+                                @endsubscriber
+                            @endsubscribed
+                        </a>
+                    @else
                         <div data-plan="{{ $plan->plan_id }}" class="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition duration-150 ease-in-out @if($plan->default){{ ' bg-gradient-to-r from-wave-600 to-indigo-500 hover:from-wave-500 hover:to-indigo-400' }}@else{{ 'bg-gray-800 hover:bg-gray-700 active:bg-gray-900 focus:border-gray-900 focus:shadow-outline-gray' }}@endif border border-transparent cursor-pointer rounded-b-md checkout focus:outline-none disabled:opacity-25">
                             @subscribed($plan->slug)
                                 Your subscribed to this plan
@@ -53,6 +64,8 @@
                                 @endsubscriber
                             @endsubscribed
                         </div>
+                    @endif 
+                        
                     </div>
 
             </div>
