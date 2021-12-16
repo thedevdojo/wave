d865212d31e4c3febe6c8cfce7e89889c207a1e0
Bobby Iliev <bobby@bobbyiliev.com>
2021-12-16 15:27:34 +0000

    Fix dev bar aplinejs v3

--- wave/src/Http/Controllers/SettingsController.php
+++ wave/src/Http/Controllers/SettingsController.php
@@ -25,8 +25,7 @@ class SettingsController extends Controller
         $request->validate([
             'name' => 'required|string',
             'email' => 'sometimes|required|email|unique:users,email,' . Auth::user()->id,
-            'username' => 'sometimes|required|unique:users,username,' . Auth::user()->id,
-	    'avatar' => 'sometimes|image'
+            'username' => 'sometimes|required|unique:users,username,' . Auth::user()->id
         ]);
 
     	$authed_user = auth()->user();
