30116eeb1ef35b15ba65a638aba35b32c7895ae4
Bobby Iliev <bobby@bobbyiliev.com>
2022-04-17 19:38:42 +0000

    Fix pgsql sequence after seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Wave\Facades\Wave;
 
 class DatabaseSeeder extends Seeder
 {
@@ -36,5 +37,22 @@ class DatabaseSeeder extends Seeder
         $this->call(VoyagerThemesTableSeeder::class);
         $this->call(VoyagerThemeOptionsTableSeeder::class);
         $this->call(WaveKeyValuesTableSeeder::class);
+        fixPostgresSequence();
+    }
+}
+
+if (!function_exists('fixPostgresSequence')) {
+
+    function fixPostgresSequence()
+    {
+        if (config('database.default') === 'pgsql') {
+            $tables = \DB::select('SELECT table_name FROM information_schema.tables WHERE table_schema = \'public\' ORDER BY table_name;');
+            foreach ($tables as $table) {
+                if (\Schema::hasColumn($table->table_name, 'id')) {
+                    $seq = \DB::table($table->table_name)->max('id') + 1;
+                    \DB::select('SELECT setval(pg_get_serial_sequence(\'' . $table->table_name . '\', \'id\'), coalesce(' . $seq . ',1), false) FROM ' . $table->table_name);
+                }
+            }
+        }
     }
 }
