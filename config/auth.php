cf79df5632b908c9835919406277568f25e0fd1e
thinkverse <hallberg.kim@gmail.com>
2022-05-11 05:23:11 +0200

    Add back api guard

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+        ],
     ],
 
     /*
