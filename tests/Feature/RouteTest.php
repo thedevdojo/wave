b88bd30ef97f357bedd3f3eecaada61d73f4cfb6
thinkverse <hallberg.kim@gmail.com>
2022-05-11 04:07:57 +0200

    Update User model reference in tests

--- tests/Feature/RouteTest.php
+++ tests/Feature/RouteTest.php
@@ -1,27 +1,17 @@
 <?php
 
 test('available routes', function ($url) {
+    $response = $this->get($url);
 
-    $appURL = env('APP_URL');
-
-        $response = $this->get($url);
-
-        $response->assertStatus(200);
-
-
+    $response->assertStatus(200);
 })->with('routes');
 
 test('available auth routes', function ($url) {
-
-    $user = \App\User::find(1);
+    $user = \App\Models\User::find(1);
 
     $this->actingAs($user);
 
-    $appURL = env('APP_URL');
+    $response = $this->get($url);
 
-        $response = $this->get($url);
-
-        $response->assertStatus(200);
-
-
+    $response->assertStatus(200);
 })->with('authroutes');
