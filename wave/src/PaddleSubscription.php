eb06a20adb90cce30484b313fabab029c3fc782e
thinkverse <hallberg.kim@gmail.com>
2022-05-12 03:30:17 +0200

    Update PaddleSubscription attributes

--- wave/src/PaddleSubscription.php
+++ wave/src/PaddleSubscription.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class PaddleSubscription extends Model
 {
-    protected $fillable = ['subscription_id', 'plan_id', 'user_id', 'status', 'next_bill_date', 'update_url', 'cancel_url', 'cancelled_at'];
+    protected $fillable = ['subscription_id', 'plan_id', 'user_id', 'status', 'update_url', 'cancel_url', 'cancelled_at', 'last_payment_at', 'next_payment_at'];
+
+    protected $casts = [
+        'last_payment_at' => 'datetime',
+        'next_payment_at' => 'datetime',
+        'cancelled_at' => 'datetime'
+    ];
 }
