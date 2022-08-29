b26e0f2865fbc094462b8f45f44d3b44dd8aa442
Bobby Iliev <bobby@bobbyiliev.com>
2022-08-29 07:44:43 +0000

    Fix a typo in plans view

--- resources/views/themes/tailwind/partials/plans.blade.php
+++ resources/views/themes/tailwind/partials/plans.blade.php
@@ -44,7 +44,7 @@
                 <div class="relative">
                         <div data-plan="{{ $plan->plan_id }}" class="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition duration-150 ease-in-out @if($plan->default){{ ' bg-gradient-to-r from-wave-600 to-indigo-500 hover:from-wave-500 hover:to-indigo-400' }}@else{{ 'bg-gray-800 hover:bg-gray-700 active:bg-gray-900 focus:border-gray-900 focus:shadow-outline-gray' }}@endif border border-transparent cursor-pointer rounded-b-md checkout focus:outline-none disabled:opacity-25">
                             @subscribed($plan->slug)
-                                Your subscribed to this plan
+                                You are subscribed to this plan
                             @notsubscribed
                                 @subscriber
                                     Switch Plans
