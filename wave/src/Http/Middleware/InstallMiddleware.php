b2fbd71273a34b92293f2195c70bf30da019cb4a
Bobby Iliev <bobby@bobbyiliev.com>
2023-01-28 17:17:14 +0200

    Fix OOM problem in install middleware (#87)

--- wave/src/Http/Middleware/InstallMiddleware.php
+++ wave/src/Http/Middleware/InstallMiddleware.php
@@ -18,7 +18,7 @@ class InstallMiddleware
     {
 
         // If there are no users in the database we need to run the install script
-        if(User::all()->count() < 1){
+        if(User::first() === null){
             if( $request->route()->getName() != 'wave.install' ){
                 return redirect()->route('wave.install');
             }
