95c5776fe4d077aefa2292855810c8c65c326661
dschiffer12 <dschiffer12@gmail.com>
2023-02-11 11:15:02 -0700

    Neat

--- themes/tallstack/welcome.blade.php
+++ themes/tallstack/welcome.blade.php
@@ -4,8 +4,8 @@
 
 	<div class="py-20 mx-auto text-center max-w-7xl">
         <div class="w-full space-y-2">
-            <h1 class="mb-5 text-5xl font-medium">Welcome Aboard!</h1>
-            <p class="py-0 my-0">Thanks for subscribing and welcome aboard.
+            <h1 class="mb-5 text-5xl font-medium">Ahoy!</h1>
+            <p class="py-0 my-0">Thanks for subscribing. We are so glad to have you!</p>
 
                 @if(Request::get('complete')){{ 'Please finish completing your profile information below.' }} @endif</p>
             <p class="py-0 my-0">This file can be modified inside of your <code class="px-2 py-1 font-mono text-base font-medium text-gray-600 bg-indigo-100 rounded-md">resources/views/{{ theme_folder('/welcome.blade.php') }}</code> file ✌️</p>
