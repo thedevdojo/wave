112dfe25d0bb38184b3d5a15418abdf448194806
dschiffer12 <dschiffer12@gmail.com>
2023-02-15 16:24:38 -0700

    Update

--- config/themes.php
+++ config/themes.php
@@ -2,7 +2,7 @@
 
 return [
 
-	'themes_folder' => resource_path('views/themes'),
+	'themes_folder' => resource_path('resources/views/themes'),
 	'publish_assets' => false
 
 ];
--- resources/views/themes/tailwind/dashboard/index.blade.php
+++ resources/views/themes/tailwind/dashboard/index.blade.php
@@ -1,9 +1,6 @@
 @extends('theme::layouts.app')
-
-
 @section('content')
-
-    <section class="bg-white">
+<section class="bg-white">
     <div class="max-w-6xl py-12 mx-auto">
         <div class="grid gap-8 md:grid-cols-2 lg:gap-12">
             <a href="#_" class="flex flex-col p-6 space-y-6 transition-all duration-500 bg-white border border-indigo-100 rounded-lg shadow hover:shadow-xl lg:p-8 lg:flex-row lg:space-y-0 lg:space-x-6">
@@ -23,14 +20,14 @@
                 </div>
             </a>
             <a href="#_" class="flex flex-col p-6 space-y-6 transition-all duration-500 bg-white border border-indigo-100 rounded-lg shadow hover:shadow-xl lg:p-8 lg:flex-row lg:space-y-0 lg:space-x-6">
-                <div class="flex items-center justify-center w-16 h-16 border border-indigo-200 shadow-inner bg-gradient-to-br from-cyan-50 to-cyan-200 rounded-xl lg:h-20 lg:w-20 tails-relative">
+                <div class="flex items-center justify-center w-16 h-16 border border-indigo-200 shadow-inner bg-gradient-to-br from-cyan-50 to-cyan-200 rounded-xl lg:h-20 lg:w-20">
                     <svg class="w-10 h-10 text-cyan-700" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" class=""></path></svg>
-                <div class="tails-hover-element">div</div></div>
+                </div>
                 <div class="flex-1">
-                    <h5 class="mt-1 mb-2 text-xl font-bold lg:text-2xl 2xl:text-cyan-800">Integrates Easily</h5>
-                    <p class="mb-6 text-lg text-gray-600">Integrate each component with your stack</p>
+                    <h5 class="mt-1 mb-2 text-xl font-bold lg:text-2xl 2xl:text-cyan-800">Products</h5>
+                    <p class="mb-6 text-lg text-gray-600">We're always working on something new!</p>
                     <span class="flex items-center text-lg font-bold 2xl:text-cyan-800">
-                        Integration Details
+                        Check out what we're up to
                         <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
                     </span>
                 </div>
