3b43b0389e8aa9d08fb3d176f728087461453556
dschiffer12 <dschiffer12@gmail.com>
2023-02-21 20:22:01 -0700

    Revert "Update index.blade.php"
    This reverts commit 13de595469a76ffab657a86e0f5a90fc96c3f2a6.

--- resources/views/themes/tailwind/dashboard/index.blade.php
+++ resources/views/themes/tailwind/dashboard/index.blade.php
@@ -4,38 +4,32 @@
 @section('content')
 
 
-<div class="max-w-6xl py-12 mx-auto">
-        <div class="grid gap-8 md:grid-cols-2 lg:gap-12">
-            <a href="#_" class="flex flex-col p-6 space-y-6 transition-all duration-500 bg-white border border-indigo-100 rounded-lg shadow hover:shadow-xl lg:p-8 lg:flex-row lg:space-y-0 lg:space-x-6">
-                <div class="flex items-center justify-center w-16 h-16 border border-cyan-200 shadow-inner bg-gradient-to-br from-cyan-50 to-cyan-200 rounded-xl lg:h-20 lg:w-20">
-                    <svg class="w-10 h-10 text-cyan-700" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" class=""></path>
-</svg>
+	<div class="flex flex-col px-8 mx-auto my-6 lg:flex-row max-w-7xl xl:px-5">
+	    <div class="flex flex-col justify-start flex-1 mb-5 overflow-hidden bg-white border rounded-lg lg:mr-3 lg:mb-0 border-gray-150">
+	        <div class="flex flex-wrap items-center justify-between p-5 bg-white border-b border-gray-150 sm:flex-no-wrap">
+				<div class="flex items-center justify-center w-12 h-12 mr-5 rounded-lg bg-wave-100">
+					<svg class="w-6 h-6 text-wave-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+				</div>
+				<div class="relative flex-1">
+	                <h3 class="text-lg font-medium leading-6 text-gray-700">
+	                    Welcome to your Dashboard
+	                </h3>
+	                <p class="text-sm leading-5 text-gray-500 mt">
+	                    Learn More Below
+	                </p>
+				</div>
 
-                </div>
-                <div class="flex-1">
-                    <h5 class="mt-1 mb-2 text-xl font-bold lg:text-2xl 2xl:text-cyan-800">Blog</h5>
-                    <p class="mb-6 text-lg text-gray-600">New ideas, products and discussions.</p>
-                    <span class="flex items-center text-lg font-bold 2xl:text-cyan-800">
-                        Check out the community!
-                        <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-                    </span>
-                </div>
-            </a>
-            <a href="#_" class="flex flex-col p-6 space-y-6 transition-all duration-500 bg-white border border-indigo-100 rounded-lg shadow hover:shadow-xl lg:p-8 lg:flex-row lg:space-y-0 lg:space-x-6">
-                <div class="flex items-center justify-center w-16 h-16 border border-indigo-200 shadow-inner bg-gradient-to-br from-cyan-50 to-cyan-200 rounded-xl lg:h-20 lg:w-20">
-                    <svg class="w-10 h-10 text-cyan-700" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" class=""></path></svg>
-                </div>
-                <div class="flex-1">
-                    <h5 class="mt-1 mb-2 text-xl font-bold lg:text-2xl 2xl:text-cyan-800">Products</h5>
-                    <p class="mb-6 text-lg text-gray-600">We're always working on something new!</p>
-                    <span class="flex items-center text-lg font-bold 2xl:text-cyan-800">
-                        Check out what we're up to
-                        <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-                    </span>
-                </div>
-            </a>
-        </div>
-    </div>
+	        </div>
+	        <div class="relative p-5">
+	            <p class="text-base leading-loose text-gray-500">This is your application <a href="{{ route('wave.dashboard') }}" class="underline text-wave-500">dashboard</a>, you can customize this view inside of <code class="px-2 py-1 font-mono text-base font-medium text-gray-600 bg-gray-100 rounded-md">{{ theme_folder('/dashboard/index.blade.php') }}</code><br><br> (Themes are located inside the <code>resources/views/themes</code> folder)</p>
+				<span class="inline-flex mt-5 rounded-md shadow-sm">
+	                <a href="{{ url('docs') }}" class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50">
+	                    Read The Docs
+	                </a>
+				</span>
+			</div>
+		</div>
+
+	</div>
 
 @endsection
