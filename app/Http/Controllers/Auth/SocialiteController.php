5f301e59f3bd81c9a41c47776a2d95b9461353a9
triptasoft <hasbullachalmughniy@gmail.com>
2023-03-30 14:25:52 +0700

    Add android auth

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -10,6 +10,7 @@ use Laravel\Socialite\Facades\Socialite;
 use TCG\Voyager\Models\Role;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class SocialiteController extends Controller
 {
@@ -75,5 +76,64 @@ class SocialiteController extends Controller
                 'message'      => $message,
                 'message_type' => 'success',
             ]);
+    }
+
+    public function auth(Request $request)
+    {
+        $google = Socialite::driver('google')->userFromToken($request->access_token);
+        // $google = Socialite::driver('google')->user();
+        $role   = Role::where('name', '=', config('voyager.user.default_role'))->first();
+
+        $trial_days    = setting('billing.trial_days', 14);
+        $trial_ends_at = null;
+
+        if (intval($trial_days) > 0) {
+            $trial_ends_at = now()->addDays(setting('billing.trial_days', 14));
+        }
+
+        $user = User::query()
+            ->where('provider_id', $google->getId())
+            ->first();
+
+        if ($user !== null) {
+            return $this->respondWithToken($user);
+        }
+
+        $user = User::query()
+                ->where('email', $google->getEmail())
+                ->first();
+
+        if ($user !== null) {
+            $user->update([
+                'provider_id' => $google->getId(),
+            ]);
+
+            return $this->respondWithToken($user);
+        }
+
+        $user = User::query()->create([
+            'provider_id'   => $google->getId(),
+            'email'         => $google->getEmail(),
+            'name'          => $google->getName(),
+            'password'      => bcrypt($google->getId()),
+            'username'      => substr($google->getEmail(), 0, strpos($google->getEmail(), '@')),
+            'verified'      => 1,
+            'trial_ends_at' => $trial_ends_at,
+            'role'          => $role->id,
+        ]);
+
+        event(new Registered($user));
+
+        return $this->respondWithToken($user);
+
+    }
+    protected function respondWithToken($user)
+    {
+        $token = JWTAuth::fromUser($user);
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => config('wave.api.auth_token_expires', 60)
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Wave::api();
+
+Route::post('auth/mobile', [App\Http\Controllers\Auth\SocialiteController::class, 'auth']);
