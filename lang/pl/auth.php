823f0fca10c67eef2576e73f806fc9bd43eb161a
Daniel Kamiński <daniel.kaminski123@gmail.com>
2023-10-17 12:44:04 +0200

    Create auth.php

---
+++ lang/pl/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Podane dane nie pasują do naszych rekordów.',
+    'password' => 'Podane hasło jest nieprawidłowe.',
+    'throttle' => 'Zbyt wiele prób logowania. Spróbuj ponownie za :seconds sekund.',
+
+];
