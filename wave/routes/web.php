2ecb6987b0a7ff598192ad4ca00d2194ea4d5cc2
Rafael Acioly <aciolyr@gmail.com>
2023-11-08 22:09:50 -0300

    reorganize web routes

--- wave/routes/web.php
+++ wave/routes/web.php
@@ -1,71 +1,109 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
+use App\Models\User;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
+use Wave\Http\Controllers\AnnouncementController;
+use Wave\Http\Controllers\Auth\LoginController;
+use Wave\Http\Controllers\BlogController;
+use Wave\Http\Controllers\DashboardController;
+use Wave\Http\Controllers\HomeController;
+use Wave\Http\Controllers\NotificationController;
+use Wave\Http\Controllers\PageController;
+use Wave\Http\Controllers\ProfileController;
+use Wave\Http\Controllers\SettingsController;
+use Wave\Http\Controllers\SubscriptionController;
 
 Route::impersonate();
-
-Route::get('/', '\Wave\Http\Controllers\HomeController@index')->name('wave.home');
-Route::get('@{username}', '\Wave\Http\Controllers\ProfileController@index')->name('wave.profile');
 
 // Documentation routes
 Route::view('docs/{page?}', 'docs::index')->where('page', '(.*)');
 
-// Additional Auth Routes
-Route::get('logout', '\Wave\Http\Controllers\Auth\LoginController@logout')->name('wave.logout');
-Route::get('user/verify/{verification_code}', '\Wave\Http\Controllers\Auth\RegisterController@verify')->name('verify');
-Route::post('register/complete', '\Wave\Http\Controllers\Auth\RegisterController@complete')->name('wave.register-complete');
-
-Route::get('blog', '\Wave\Http\Controllers\BlogController@index')->name('wave.blog');
-Route::get('blog/{category}', '\Wave\Http\Controllers\BlogController@category')->name('wave.blog.category');
-Route::get('blog/{category}/{post}', '\Wave\Http\Controllers\BlogController@post')->name('wave.blog.post');
-
-Route::view('install', 'wave::install')->name('wave.install');
-
-/***** Pages *****/
-Route::get('p/{page}', '\Wave\Http\Controllers\PageController@page');
-
-/***** Pricing Page *****/
 Route::view('pricing', 'theme::pricing')->name('wave.pricing');
 
-/***** Billing Routes *****/
+// Billing Routes
 Route::post('paddle/webhook', '\Wave\Http\Controllers\WebhookController');
-Route::post('checkout', '\Wave\Http\Controllers\SubscriptionController@checkout')->name('checkout');
 
-Route::get('test', '\Wave\Http\Controllers\SubscriptionController@test');
+Route::get('p/{page}', [PageController::class, 'page']);
+
+// Additional Auth Routes
+Route::get('logout', [LoginController::class, 'logout'])->name('wave.logout');
+
+Route::get('/', [HomeController::class, 'index'])->name('wave.home');
+
+Route::get('@{username}', [ProfileController::class, 'index'])->name('wave.profile');
+
+Route::get('install', function () {
+    if (!User::first()->exists()) {
+        Artisan::command('db:seed', ['--force' => true]);
+        Artisan::call('storage:link');
+    }
+
+    return view('wave::install');
+})->name('wave.install');
+
+Route::controller(RegisterController::class)->group(function () {
+    Route::get('user/verify/{verification_code}', 'verify')->name('wave.register');
+    Route::post('register/complete', 'complete')->name('wave.register-complete');;
+});
+
+Route::controller(BlogController::class)->prefix('blog')->group(function () {
+    Route::get('', 'index')->name('wave.blog');
+    Route::get('{category}', 'category')->name('wave.blog.category');
+    Route::get('{category}/{post}', 'post')->name('wave.blog.post');
+});
+
+Route::controller(SubscriptionController::class)->group(function () {
+    Route::get('test', 'test');
+    Route::post('checkout', 'checkout')->name('checkout');
+});
 
 Route::group(['middleware' => 'wave'], function () {
-	Route::get('dashboard', '\Wave\Http\Controllers\DashboardController@index')->name('wave.dashboard');
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('wave.dashboard');
 });
 
-Route::group(['middleware' => 'auth'], function(){
-	Route::get('settings/{section?}', '\Wave\Http\Controllers\SettingsController@index')->name('wave.settings');
-
-	Route::post('settings/profile', '\Wave\Http\Controllers\SettingsController@profilePut')->name('wave.settings.profile.put');
-	Route::put('settings/security', '\Wave\Http\Controllers\SettingsController@securityPut')->name('wave.settings.security.put');
-
-	Route::post('settings/api', '\Wave\Http\Controllers\SettingsController@apiPost')->name('wave.settings.api.post');
-	Route::put('settings/api/{id?}', '\Wave\Http\Controllers\SettingsController@apiPut')->name('wave.settings.api.put');
-	Route::delete('settings/api/{id?}', '\Wave\Http\Controllers\SettingsController@apiDelete')->name('wave.settings.api.delete');
-
-	Route::get('settings/invoices/{invoice}', '\Wave\Http\Controllers\SettingsController@invoice')->name('wave.invoice');
-
-	Route::get('notifications', '\Wave\Http\Controllers\NotificationController@index')->name('wave.notifications');
-	Route::get('announcements', '\Wave\Http\Controllers\AnnouncementController@index')->name('wave.announcements');
-	Route::get('announcement/{id}', '\Wave\Http\Controllers\AnnouncementController@announcement')->name('wave.announcement');
-	Route::post('announcements/read', '\Wave\Http\Controllers\AnnouncementController@read')->name('wave.announcements.read');
-	Route::get('notifications', '\Wave\Http\Controllers\NotificationController@index')->name('wave.notifications');
-	Route::post('notification/read/{id}', '\Wave\Http\Controllers\NotificationController@delete')->name('wave.notification.read');
-
-    /********** Checkout/Billing Routes ***********/
-    Route::post('cancel', '\Wave\Http\Controllers\SubscriptionController@cancel')->name('wave.cancel');
-    Route::view('checkout/welcome', 'theme::welcome');
-
-    Route::post('subscribe', '\Wave\Http\Controllers\SubscriptionController@subscribe')->name('wave.subscribe');
-	Route::view('trial_over', 'theme::trial_over')->name('wave.trial_over');
-	Route::view('cancelled', 'theme::cancelled')->name('wave.cancelled');
-    Route::post('switch-plans', '\Wave\Http\Controllers\SubscriptionController@switchPlans')->name('wave.switch-plans');
-});
-
-Route::group(['middleware' => 'admin.user'], function(){
+Route::group(['middleware' => 'admin.user'], function () {
     Route::view('admin/do', 'wave::do');
+});
+
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::view('checkout/welcome', 'theme::welcome');
+    Route::view('trial_over', 'theme::trial_over')->name('wave.trial_over');
+    Route::view('cancelled', 'theme::cancelled')->name('wave.cancelled');
+
+    Route::controller(SettingsController::class)
+        ->prefix('settings')
+        ->group(function () {
+            Route::get('{section?}', 'index')->name('wave.settings');
+            Route::post('profile', 'profilePut')->name('wave.settings.profile.put');
+            Route::put('security', 'securityPut')->name('wave.settings.security.put');
+            Route::post('api', 'apiPost')->name('wave.settings.api.post');
+            Route::put('api/{id?}', 'apiPut')->name('wave.settings.api.put');
+            Route::delete('api/{id?}', 'apiDelete')->name('wave.settings.api.delete');
+            Route::get('invoices/{invoice}', 'invoice')->name('wave.invoice');
+        });
+
+    Route::controller(AnnouncementController::class)
+        ->prefix('announcements')
+        ->group(function () {
+            Route::get('', 'index')->name('wave.announcements');
+            Route::get('{id}', 'announcement')->name('wave.announcement');
+            Route::post('read', 'read')->name('wave.announcements.read');
+        });
+
+    Route::controller(NotificationController::class)
+        ->prefix('notifications')
+        ->group(function () {
+            Route::get('', 'index')->name('wave.notifications');
+            Route::post('read/{id}', 'delete')->name('wave.notification.read');
+        });
+
+    Route::controller(SubscriptionController::class)
+        ->group(function () {
+            Route::post('subscribe', 'subscribe')->name('wave.subscribe');
+            Route::post('switch-plans', 'switchPlans')->name('wave.switch-plans');
+            Route::post('cancel', 'cancel')->name('wave.cancel');
+        });
 });
