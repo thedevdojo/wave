6c0b10e529677f34e601f1dd30ece4751ad27ecd
Rafael Acioly <aciolyr@gmail.com>
2023-11-08 23:00:54 -0300

    fix-queries-to-read-announcements

--- wave/src/Http/Controllers/AnnouncementController.php
+++ wave/src/Http/Controllers/AnnouncementController.php
@@ -3,27 +3,32 @@
 namespace Wave\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Database\Query\Builder;
 use Wave\Announcement;
 
 class AnnouncementController extends Controller
 {
-    public function index(){
-    	$announcements = Announcement::paginate(10);
+    public function index()
+    {
+        $announcements = Announcement::paginate(10);
         return view('theme::announcements.index', compact('announcements'));
     }
 
-    public function announcement($id){
+    public function announcement($id)
+    {
         $announcement = Announcement::find($id);
-    	return view('theme::announcements.announcement', compact('announcement'));
+        return view('theme::announcements.announcement', compact('announcement'));
     }
 
-    public function read(){
-    	$user = auth()->user();
-    	$announcements = Announcement::all();
-    	foreach($announcements as $announcement){
-            if(!$user->announcements()->where('id', $announcement->id)->exists()){
-    		  $user->announcements()->attach($announcement->id);
-            }
-    	}
+    public function read()
+    {
+        $user = auth()->user();
+        Announcement::whereDoesntHave('users', function ($query) use ($user) {
+            $query->where('user_id', $user->id);
+        })->get()
+            ->pluck('id')
+            ->tap(function ($missingAnnouncements) use ($user) {
+                $user->announcements()->attach($missingAnnouncements->toArray());
+            });
     }
 }
