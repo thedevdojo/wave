e0148f18d845b3d5a335e0b78d9ff6d8c6f34beb
Bobby Iliev <bobby@bobbyiliev.com>
2024-03-25 22:04:58 +0000

    Fix the cancel and update buttons

--- wave/src/Http/Controllers/SubscriptionController.php
+++ wave/src/Http/Controllers/SubscriptionController.php
@@ -86,102 +86,93 @@ class SubscriptionController extends Controller
     }
 
     public function checkout(Request $request){
-        $response = Http::withToken($this->vendor_auth_code)->get($this->paddle_url . '/transactions/' . $request->checkout_id);
+        $retryCount = 5;
+        $initialDelay = 2;
+        $transaction = null;
         $status = 0;
         $message = '';
         $guest = (auth()->guest()) ? 1 : 0;
     
-        if($response->successful()){
-            $resBody = json_decode($response->body());
-     
-            if(isset($resBody->data->status)){
-                $transaction = $resBody->data;
-                if (is_null($transaction->subscription_id)) {
-                    sleep(4);
-                    $response = Http::withToken($this->vendor_auth_code)->get($this->paddle_url . '/transactions/' . $request->checkout_id);
-                    $resBody = json_decode($response->body());
+        for ($i = 0; $i < $retryCount; $i++) {
+            $response = Http::withToken($this->vendor_auth_code)->get($this->paddle_url . '/transactions/' . $request->checkout_id);
+    
+            if ($response->successful()) {
+                $resBody = json_decode($response->body());
+                if (isset($resBody->data->status) && !is_null($resBody->data->subscription_id)) {
                     $transaction = $resBody->data;
+                    break;
                 }
-                $plans = Plan::all();
-
-                if($transaction->origin === "web" && $plans->contains('plan_id', $transaction->items[0]->price->id)){
-                    $subscriptionUser = Http::withToken($this->vendor_auth_code)->get($this->paddle_url . '/subscriptions/' . $transaction->subscription_id);
-                    $subscriptionData = json_decode($subscriptionUser->body());
-                    if (isset($resBody->data)) {
-                        $subscription = $subscriptionData->data;
+            }
+    
+            sleep($initialDelay * (2 ** $i));
+        }
+    
+        if ($transaction) {
+            // Proceed with processing the transaction
+            $plans = Plan::all();
+            if($transaction->origin === "web" && $plans->contains('plan_id', $transaction->items[0]->price->id)){
+                $subscriptionUser = Http::withToken($this->vendor_auth_code)->get($this->paddle_url . '/subscriptions/' . $transaction->subscription_id);
+                $subscriptionData = json_decode($subscriptionUser->body());
+                $subscription = $subscriptionData->data;
+    
+                $customerResponse = Http::withToken($this->vendor_auth_code)->get($this->paddle_url . '/customers/' . $subscription->customer_id);
+                $customerData = json_decode($customerResponse->body());
+                $customerEmail = $customerData->data->email;
+                $customerName = $customerData->data->name;
+                if (empty($customerName)) {
+                    $nameParts = explode('@', $customerEmail);
+                    $customerName = $nameParts[0];
+                }
+    
+                if($guest){
+                    if(User::where('email', $customerEmail)->exists()){
+                        $user = User::where('email', $customerEmail)->first();
                     } else {
-                        // Retry fetching the subscription data
-                        sleep(4);
-                        $subscriptionUser = Http::withToken($this->vendor_auth_code)->get($this->paddle_url . '/subscriptions/' . $transaction->subscription_id);
-                        $subscriptionData = json_decode($subscriptionUser->body());
-                        $subscription = $subscriptionData->data;
+                        $registration = new \Wave\Http\Controllers\Auth\RegisterController;
+                        $user_data = [
+                            'name' => $customerName,
+                            'email' => $customerEmail,
+                            'password' => Hash::make(uniqid())
+                        ];
+                        $user = $registration->create($user_data);
+                        Auth::login($user);
                     }
-
-                    // Fetch customer's details using the customer_id
-                    $customerResponse = Http::withToken($this->vendor_auth_code)->get($this->paddle_url . '/customers/' . $subscription->customer_id);
-                    $customerData = json_decode($customerResponse->body());
-                    $customerEmail = $customerData->data->email;
-                    $customerName = $customerData->data->name;
-                    // Check if the name is null or empty
-                    if (empty($customerName)) {
-                        // Extract the part of the email address before the '@' symbol
-                        $nameParts = explode('@', $customerEmail);
-                        $customerName = $nameParts[0];
-                    }
-
-                    if(auth()->guest()){
-                        if(User::where('email', $customerEmail)->exists()){
-                            $user = User::where('email', $customerEmail)->first();
-                        } else {
-                            // create a new user
-                            $registration = new \Wave\Http\Controllers\Auth\RegisterController;
-                            $user_data = [
-                                'name' => $customerName,
-                                'email' => $customerEmail,
-                                'password' => Hash::make(uniqid())
-                            ];
-                            $user = $registration->create($user_data);
-                            Auth::login($user);
-                        }
-                    } else {
-                        $user = auth()->user();
-                    }
-                
-                    $plan = Plan::where('plan_id', $transaction->items[0]->price->id)->first();       
-    
-                    // add associated role to user
-                    $user->role_id = $plan->role_id;
-                    $user->save();
-    
-                    $subscription = PaddleSubscription::create([
-                        'subscription_id' => $transaction->subscription_id,
-                        'plan_id' => $transaction->items[0]->price->product_id,
-                        'user_id' => $user->id,
-                        'status' => $subscription->status,
-                        'last_payment_at' => $subscription->first_billed_at,
-                        'next_payment_at' => $subscription->next_billed_at,
-                        'cancel_url' => $subscription->management_urls->cancel,
-                        'update_url' => $subscription->management_urls->update_payment_method
-                    ]);
-    
-                    $status = 1;
                 } else {
-                    $message = 'Error locating that subscription product id. Please contact us if you think this is incorrect.';
+                    $user = auth()->user();
                 }
+            
+                $plan = Plan::where('plan_id', $transaction->items[0]->price->id)->first();       
+    
+                // Update user role based on plan
+                $user->role_id = $plan->role_id;
+                $user->save();
+    
+                // Create or update subscription details
+                $subscriptionRecord = PaddleSubscription::create([
+                    'subscription_id' => $transaction->subscription_id,
+                    'plan_id' => $transaction->items[0]->price->product_id,
+                    'user_id' => $user->id,
+                    'status' => $subscription->status,
+                    'last_payment_at' => $subscription->first_billed_at,
+                    'next_payment_at' => $subscription->next_billed_at,
+                    'cancel_url' => $subscription->management_urls->cancel,
+                    'update_url' => $subscription->management_urls->update_payment_method
+                ]);
+    
+                $status = 1;
             } else {
-                $message = 'Error locating that order. Please contact us if you think this is incorrect.';
+                $message = 'Error locating that subscription product id. Please contact us if you think this is incorrect.';
             }
         } else {
-            $message = $response->serverError();
+            $message = 'Error processing the transaction. Please try again.';
         }
     
         return response()->json([
-                    'status' => $status,
-                    'message' => $message,
-                    'guest' => $guest
-                ]);
-    }
-    
+            'status' => $status,
+            'message' => $message,
+            'guest' => $guest
+        ]);
+    }    
 
     public function invoices(User $user){
 
