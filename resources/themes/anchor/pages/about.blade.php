760ef3e275de0a05c3e25c6942434c09dacb4a04
Tony Lea <tnylea@gmail.com>
2024-09-06 10:21:01 -0400

    Adding everything back including the correct profile page

---
+++ resources/themes/anchor/pages/about.blade.php
@@ -0,0 +1,17 @@
+<?php
+    use function Laravel\Folio\{name};
+    name('about');
+?>
+
+<x-layouts.marketing
+    :seo="[
+        'title'         => setting('site.title', 'Laravel Wave'),
+        'description'   => setting('site.description', 'Software as a Service Starter Kit'),
+        'image'         => url('/og_image.png'),
+        'type'          => 'website'
+    ]"
+>
+        
+       About Page
+
+</x-layouts.marketing>
