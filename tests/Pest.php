e7f655847ee93455de0832f503046ec2fd27fefb
Bobby Iliev <bobby@bobbyiliev.com>
2024-09-11 23:00:58 +0300

    Upgrade to Pest v3 (#130)

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,7 +1,5 @@
 <?php
 
-use Tests\Traits\RecursiveRefreshDatabase as RefreshDatabase;
-
 /*
 |--------------------------------------------------------------------------
 | Test Case
@@ -9,11 +7,13 @@ use Tests\Traits\RecursiveRefreshDatabase as RefreshDatabase;
 |
 | The closure you provide to your test functions is always bound to a specific PHPUnit test
 | case class. By default, that class is "PHPUnit\Framework\TestCase". Of course, you may
-| need to change it using the "uses()" function to bind a different classes or traits.
+| need to change it using the "pest()" function to bind a different classes or traits.
 |
 */
 
-uses(Tests\TestCase::class, RefreshDatabase::class)->in('Feature');
+pest()->extend(Tests\TestCase::class)
+    ->extend(Tests\Traits\RecursiveRefreshDatabase::class)
+    ->in('Feature');
 
 /*
 |--------------------------------------------------------------------------
