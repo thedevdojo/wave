2bcd1eb4e8c6baef49de2bc93d7b55aab985d423
Tony Lea <tnylea@gmail.com>
2024-09-28 11:30:14 -0400

    Adding updatesg

--- resources/themes/anchor/components/elements/card.blade.php
+++ resources/themes/anchor/components/elements/card.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->twMerge('bg-white dark:bg-zinc-900 lg:p-8 rounded-lg relative border-0 lg:border h-auto w-full dark:border-zinc-800 border-zinc-200/50') }}>
+<div {{ $attributes->twMerge('bg-white dark:bg-zinc-900 lg:p-8 rounded-lg relative lg:border h-auto w-full dark:border-zinc-800 border-zinc-200/50') }}>
     {{ $slot }}
 </div>
