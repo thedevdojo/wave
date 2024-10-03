f07a74ed059eadd856aefed7356247b4ae2dba63
Tony Lea <tnylea@gmail.com>
2024-10-03 11:34:49 -0400

    updating default theme

--- resources/themes/anchor/components/marketing/elements/header.blade.php
+++ resources/themes/anchor/components/marketing/elements/header.blade.php
@@ -43,7 +43,7 @@
             <div class="z-20 flex items-center justify-between w-full md:w-auto">
                 <div class="relative z-20 inline-flex">
                     <a href="{{ route('home') }}" class="flex items-center justify-center space-x-3 font-bold text-zinc-900">
-                    <x-logo class="w-auto h-8"></x-logo>
+                    <x-logo class="w-auto h-8 md:h-9"></x-logo>
                     </a>
                 </div>
                 <div class="flex justify-end flex-grow md:hidden">
@@ -59,12 +59,12 @@
                     <li x-data="{ open: false }" @mouseenter="showOverlay=true" @mouseleave="showOverlay=false" class="z-30 flex flex-col items-start h-auto border-b border-gray-100 md:h-full md:border-b-0 group md:flex-row md:items-center">
                         <a href="#_" x-on:click="open=!open" class="flex items-center w-full h-16 gap-1 text-sm font-semibold text-gray-700 transition duration-300 hover:bg-gray-100 md:hover:bg-transparent px-7 md:h-full md:px-0 md:w-auto hover:text-gray-900">
                             <span class="">Platform</span>
-                            <svg class="w-5 h-5 transition-all duration-300 ease-out group-hover:-rotate-180" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" class=""></path></svg>
+                            <svg :class="{ 'group-hover:-rotate-180' : !mobileMenuOpen, '-rotate-180' : mobileMenuOpen && open }" class="w-5 h-5 transition-all duration-300 ease-out" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" class=""></path></svg>
                         </a>
                         <div 
                             :class="{ 'hidden md:block opacity-0 invisible md:absolute' : !open, 'md:invisible md:opacity-0 md:hidden md:absolute' : open }"
                             class="top-0 left-0 w-screen space-y-3 transition-transform duration-300 ease-out bg-white border-t border-b border-gray-100 md:shadow-md md:-translate-y-2 md:mt-24 md:block md:group-hover:block md:group-hover:visible md:group-hover:opacity-100 md:group-hover:translate-y-0">
-                            <ul class="flex flex-col justify-between max-w-6xl mx-auto md:px-12 md:flex-row">
+                            <ul class="flex flex-col justify-between mx-auto max-w-7xl md:px-16 md:flex-row">
                                 <li class="w-full border-l border-gray-100 md:w-1/5">
                                     <a href="#_" onclick="demoButtonClickMessage(event)" class="block h-full p-6 text-lg font-semibold hover:bg-gray-50 lg:p-7 lg:py-10">
                                         <img src="/wave/img/icons/anchor.png" class="w-12 h-auto" alt="feature 1 icon" />
@@ -106,12 +106,12 @@
                     <li x-data="{ open: false }" @mouseenter="showOverlay=true" @mouseleave="showOverlay=false" class="z-30 flex flex-col items-start h-auto border-b border-gray-100 md:h-full md:border-b-0 group md:flex-row md:items-center">
                         <a href="#_" x-on:click="open=!open" class="flex items-center w-full h-16 gap-1 text-sm font-semibold text-gray-700 transition duration-300 hover:bg-gray-100 md:hover:bg-transparent px-7 md:h-full md:px-0 md:w-auto hover:text-gray-900">
                             <span class="">Resources</span>
-                            <svg class="w-5 h-5 transition-all duration-300 ease-out group-hover:-rotate-180" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" class=""></path></svg>
+                            <svg :class="{ 'group-hover:-rotate-180' : !mobileMenuOpen, '-rotate-180' : mobileMenuOpen && open }" class="w-5 h-5 transition-all duration-300 ease-out" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" class=""></path></svg>
                         </a>
                         <div 
                             :class="{ 'hidden md:block opacity-0 invisible md:absolute' : !open, 'md:invisible md:opacity-0 md:hidden md:absolute' : open }"
                             class="top-0 left-0 w-screen space-y-3 transition-transform duration-300 ease-out bg-white border-t border-b border-gray-100 md:shadow-md md:-translate-y-2 md:mt-24 md:block md:group-hover:block md:group-hover:visible md:group-hover:opacity-100 md:group-hover:translate-y-0">
-                            <ul class="flex flex-col justify-between max-w-6xl mx-auto md:flex-row md:px-12">
+                            <ul class="flex flex-col justify-between mx-auto max-w-7xl md:flex-row md:px-12">
                                 <div class="flex flex-col w-full border-l border-r divide-x md:flex-row divide-zinc-100 border-zinc-100">
                                     <div class="w-auto divide-y divide-zinc-100">
                                         <a href="#_" onclick="event.preventDefault(); new FilamentNotification().title('Modify this button in your theme folder').icon('heroicon-o-pencil-square').iconColor('info').send()" class="block text-sm p-7 hover:bg-neutral-100 group">
--- resources/themes/anchor/components/marketing/sections/hero.blade.php
+++ resources/themes/anchor/components/marketing/sections/hero.blade.php
@@ -1,12 +1,12 @@
-<section class="relative top-0 flex flex-col items-center justify-center w-full min-h-screen -mt-24 lg:min-h-screen">
+<section class="relative top-0 flex flex-col items-center justify-center w-full min-h-screen -mt-24 bg-white lg:min-h-screen">
     
-        <div class="flex flex-col items-center justify-between flex-1 w-full max-w-2xl gap-6 px-8 pt-32 mx-auto text-left md:px-12 xl:px-20 lg:pt-32 lg:pb-16 lg:max-w-6xl lg:flex-row">
+        <div class="flex flex-col items-center justify-between flex-1 w-full max-w-2xl gap-6 px-8 pt-32 mx-auto text-left md:px-12 xl:px-20 lg:pt-32 lg:pb-16 lg:max-w-7xl lg:flex-row">
             <div class="w-full lg:w-1/2">
-                <h1 class="font-bold tracking-tighter text-left text-7xl md:text-8xl lg:text-7xl sm:text-center lg:text-left text-zinc-900 text-balance">
-                    Your <br class="lg:block sm:hidden">Headline<br class="lg:block sm:hidden"> <span >Goes Here</span>
+                <h1 class="font-bold tracking-tighter text-left text-7xl md:text-8xl sm:text-center lg:text-left text-zinc-900 text-balance">
+                    <span class="block origin-left lg:scale-90 text-nowrap">Ship In Days</span> <span class="pr-4 text-transparent text-neutral-600 bg-clip-text bg-gradient-to-b from-neutral-900 to-neutral-500">Not Weeks</span>
                 </h1>
-                <p class="mx-auto mt-5 text-2xl font-normal text-left sm:max-w-md lg:ml-0 lg:max-w-sm sm:text-center lg:text-left text-zinc-500">
-                    Customize this to highlight your product’s unique selling points.
+                <p class="mx-auto mt-5 text-2xl font-normal text-left sm:max-w-md lg:ml-0 lg:max-w-md sm:text-center lg:text-left text-zinc-500">
+                    Customize this to highlight your product’s unique selling points<span class="hidden sm:inline"> and features</span>.
                 </p>
                 <div class="flex flex-col items-center justify-center gap-2 mx-auto mt-8 lg:justify-start md:ml-0 md:flex-row">
                     <x-button size="lg" class="w-full lg:w-auto">Primary Button</x-button>
@@ -14,11 +14,11 @@
                 </div>
             </div>
             <div class="flex items-center justify-center w-full mt-12 lg:w-1/2 lg:mt-0">
-                <img alt="Wave Character" class="relative w-full lg:scale-125" src="/wave/img/character.png" style="max-width:450px;">
+                <img alt="Wave Character" class="relative w-full lg:scale-125 xl:scale-[1.4] xl:translate-x-6" src="/wave/img/character.png" style="max-width:450px;">
             </div>
         </div>
-        <div class="flex-shrink-0 lg:h-[150px] flex border-t border-zinc-200 items-center w-full bg-zinc-100">
-            <div class="grid h-auto max-w-6xl grid-cols-1 px-8 py-10 mx-auto space-y-5 divide-y lg:space-y-0 lg:divide-y-0 divide-zinc-200 lg:py-0 lg:divide-x md:px-12 lg:px-20 lg:divide-zinc-200 lg:grid-cols-3">
+        <div class="flex-shrink-0 lg:h-[150px] flex border-t border-zinc-200 items-center w-full bg-white">
+            <div class="grid h-auto grid-cols-1 px-8 py-10 mx-auto space-y-5 divide-y max-w-7xl lg:space-y-0 lg:divide-y-0 divide-zinc-200 lg:py-0 lg:divide-x md:px-12 lg:px-20 lg:divide-zinc-200 lg:grid-cols-3">
                 <div class="">
                     <h3 class="flex items-center font-medium text-zinc-900">
                         Key Feature
