e576f3f1a1ecd9816d1559a52f4dbd7382c91ff7
Bobby Iliev <bobby@bobbyiliev.com>
2025-05-01 10:44:51 +0300

    Fix docs link to blade directives

--- resources/themes/anchor/components/app/message-for-admin.blade.php
+++ resources/themes/anchor/components/app/message-for-admin.blade.php
@@ -1,4 +1,4 @@
-<p>You're logged in with an <strong>Admin</strong> role. Did you know that you can use <a href="https://devdojo.com/wave/docs/blade-directives" target="_blank" class="underline">custom blade directives</a> to check if a user is an admin user</p>
+<p>You're logged in with an <strong>Admin</strong> role. Did you know that you can use <a href="https://devdojo.com/wave/docs/concepts/blade-directives" target="_blank" class="underline">custom blade directives</a> to check if a user is an admin user</p>
 <pre class="p-5 overflow-x-auto font-mono text-xs text-gray-600 border rounded-lg bg-gray-50 dark:bg-zinc-700/50 dark:border-gray-600 dark:text-gray-100 dark:border-gray-800 border-gray-200/80"><code>&#64;admin
     &lt;p&gt;Only you as an admin will see this message&lt;/p&gt;
 &#64;notadmin</code></pre>
--- resources/themes/anchor/components/app/message-for-subscriber.blade.php
+++ resources/themes/anchor/components/app/message-for-subscriber.blade.php
@@ -1,11 +1,11 @@
-<div class="relative w-full bg-white rounded-md border border-zinc-200 dark:bg-zinc-900 dark:border-zinc-800">
+<div class="relative w-full bg-white border rounded-md border-zinc-200 dark:bg-zinc-900 dark:border-zinc-800">
     <div class="p-5">
     <p class="mt-2">Did you know that you can use custom blade directives to check if a user is a subscriber, like the following:</p>
     </div>
-    <pre class="p-5 font-mono text-xs text-zinc-600 bg-zinc-50 border-t border-b dark:bg-black dark:text-zinc-100 dark:border-zinc-800 border-zinc-200/80"><code>&#64;subscriber
+    <pre class="p-5 font-mono text-xs border-t border-b text-zinc-600 bg-zinc-50 dark:bg-black dark:text-zinc-100 dark:border-zinc-800 border-zinc-200/80"><code>&#64;subscriber
     &lt;p&gt;You are a paid subscriber&lt;/p&gt;
 &#64;endsubscriber</code></pre>
     <div class="p-5">
-        <p><a href="https://devdojo.com/wave/docs/blade-directives" class="underline">Click here to learn more</a> about Blade directives you can use in your Wave application.</p>
+        <p><a href="https://devdojo.com/wave/docs/concepts/blade-directives" class="underline">Click here to learn more</a> about Blade directives you can use in your Wave application.</p>
     </div>
 </div>
