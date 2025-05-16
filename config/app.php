0dbfb4fe7216c4a0ceceb3f899a7dc781504c49d
Tony Lea <tnylea@gmail.com>
2025-05-16 10:32:29 -0400

    Adding fix so that way volt components get added to the default livewire dir

--- config/app.php
+++ config/app.php
@@ -202,6 +202,7 @@ return [
         App\Providers\FolioServiceProvider::class,
 
         \DevDojo\Themes\ThemesServiceProvider::class,
+        App\Providers\VoltServiceProvider::class,
 
     ],
 
