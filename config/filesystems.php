aff7e3bdd9d92be72bcfc877b3afeac5b652823e
Shift <shift@laravelshift.com>
2025-06-13 16:52:00 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files easier, Shift defaulted them and merged your true customizations - where ENV variables may not be used.

--- config/filesystems.php
+++ config/filesystems.php
@@ -7,6 +7,8 @@ return [
             'driver' => 'local',
             'root' => storage_path('app'),
             'throw' => false,
+            'serve' => true,
+            'report' => false,
         ],
     ],
 
