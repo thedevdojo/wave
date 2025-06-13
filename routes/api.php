f68497c1347bf042b7ba487376935ad3ae15be55
Shift <shift@laravelshift.com>
2025-06-13 06:46:00 +0000

    Convert route options to fluent methods
    Laravel 8 adopts the tuple syntax for controller actions. Since the old options array is incompatible with this syntax, Shift converted them to use modern, fluent methods.

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Wave::api();
 
 // Posts Example API Route
-Route::group(['middleware' => 'auth:api'], function () {
+Route::middleware('auth:api')->group(function () {
     Route::get('/posts', '\App\Http\Controllers\Api\ApiController@posts');
 });
