5c8bd78ea9369b7994b9a72bf400a1cc8a9a9b60
Blessed Ndoro <blestab@users.noreply.github.com>
2025-07-05 11:51:02 +1000

    Fix wave version display
    Update wave-info-widget.blade.php to fix wave version display using local wave.json instead of Composer InstalledVersions

--- wave/resources/views/widgets/wave-info-widget.blade.php
+++ wave/resources/views/widgets/wave-info-widget.blade.php
@@ -11,8 +11,7 @@
                 </a>
 
                 <p class="mt-2 text-xs text-gray-500 dark:text-gray-400">
-                    Show Version Here.
-                    {{-- {{ \Composer\InstalledVersions::getPrettyVersion('devdojo/wave') }} --}}
+                    {{ wave_version() }}
                 </p>
             </div>
 
