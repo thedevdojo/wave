aee9c979532b809798551fd40e18ec0287a7ee8f
Cristian Stef <84433169+crstef@users.noreply.github.com>
2025-07-24 09:03:42 +0000

    try

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,5 +1,5 @@
 <img 
     {{ $attributes->merge(['class' => 'text-gray-900 dark:text-white']) }} 
-    src="{{ asset('public/favicon.png') }}" 
+    src="{{ asset('favicon.png') }}" 
     alt="Logo" 
 />
