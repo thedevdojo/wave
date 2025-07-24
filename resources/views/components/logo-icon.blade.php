ea0b3269adc631132fb1e0f492594d6c528a64d0
Cristian Stef <84433169+crstef@users.noreply.github.com>
2025-07-24 13:08:38 +0000

    bigger logo

--- resources/views/components/logo-icon.blade.php
+++ resources/views/components/logo-icon.blade.php
@@ -3,7 +3,7 @@
 <link rel="icon" href="{{ setting('site.favicon_dark', '/wave/favicon-dark.png') }}" type="image/png" media="(prefers-color-scheme: dark)">
 {{-- Optionally, you can use the favicon as an image: --}}
 <img 
-    {{ $attributes->merge(['class' => 'text-gray-900 dark:text-white']) }} 
+    {{ $attributes->merge(['class' => 'text-gray-900 dark:text-white', 'style' => 'width:64px;height:64px;']) }} 
     src="{{ setting('site.favicon', '/wave/favicon.png') }}" 
     alt="Logo" 
 />
