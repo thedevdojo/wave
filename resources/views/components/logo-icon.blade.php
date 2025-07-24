f9c0278c7172d2beff01ef6bb6427574c7b814ee
Cristian Stef <84433169+crstef@users.noreply.github.com>
2025-07-24 09:17:59 +0000

    try

--- resources/views/components/logo-icon.blade.php
+++ resources/views/components/logo-icon.blade.php
@@ -2,5 +2,4 @@
     {{ $attributes->merge(['class' => 'text-gray-900 dark:text-white']) }} 
     src="{{ asset('public/favicon.png') }}" 
     alt="Logo" 
-    onerror="this.onerror=null;this.src='https://via.placeholder.com/32';alert('Logo image not found');"
 />
