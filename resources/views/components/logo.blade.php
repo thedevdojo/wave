bb3e998c114503dfe1a2dfcee02fc67a2aca8e75
Cristian Stef <84433169+crstef@users.noreply.github.com>
2025-07-24 09:06:24 +0000

    try

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -2,4 +2,5 @@
     {{ $attributes->merge(['class' => 'text-gray-900 dark:text-white']) }} 
     src="{{ asset('favicon.png') }}" 
     alt="Logo" 
+    onerror="this.onerror=null;this.src='https://via.placeholder.com/32';"
 />
