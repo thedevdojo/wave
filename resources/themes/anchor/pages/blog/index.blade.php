5e1a534893a50600ee2cfcc69661abd61989e52f
Patrick Brisacier <pbrisacier@mncc.fr>
2025-10-15 10:32:09 +0200

    Display only published posts

--- resources/themes/anchor/pages/blog/index.blade.php
+++ resources/themes/anchor/pages/blog/index.blade.php
@@ -2,7 +2,7 @@
     use function Laravel\Folio\{name};
     name('blog');
 
-    $posts = \Wave\Post::orderBy('created_at', 'DESC')->paginate(6);
+    $posts = \Wave\Post::where('status', 'PUBLISHED')->orderBy('created_at', 'DESC')->paginate(6);
     $categories = \Wave\Category::all();
 ?>
 
