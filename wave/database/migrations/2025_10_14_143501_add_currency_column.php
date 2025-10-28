5a367c29b0d1f760a9a899e893e1456b0e9ba96b
Patrick Brisacier <pbrisacier@mncc.fr>
2025-10-28 15:37:55 +0100

    Plan currency with 3 characters max

--- wave/database/migrations/2025_10_14_143501_add_currency_column.php
+++ wave/database/migrations/2025_10_14_143501_add_currency_column.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('plans', function (Blueprint $table) {
-            $table->string('currency')->default('$');
+            $table->string('currency', 3)->default('$');
         });
     }
 
