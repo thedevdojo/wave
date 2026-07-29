9031fce2d55b7bc405922ee42bf8ca7579661f91
Suraj Raika <surajraika5sr@gmail.com>
2026-07-29 17:03:15 +0000

    fix: Update environment check to support both production and local for HTTPS enforcement

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -40,10 +40,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(\Illuminate\Routing\UrlGenerator $url): void
     {
-        if ($this->app->environment() == 'production') {
-            $this->app['request']->server->set('HTTPS', true);
-            $url->forceScheme('https');
-        }
+        if ($this->app->environment(['production', 'local'])) {
+        $this->app['request']->server->set('HTTPS', true);
+        $url->forceScheme('https');
+    }
 
         $this->setSchemaDefaultLength();
 
